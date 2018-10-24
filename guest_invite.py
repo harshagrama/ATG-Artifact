d28c7aebabc61d11210149dc57f8a9c0c92fecdb
Harshendrakumar N N <harshag@slkconsult.com>
2018-10-24 08:23:23 +0100

    Initial revision

---
+++ guest_invite.py
@@ -0,0 +1,13 @@
+import requests 
+
+#api-endpoint
+URL = "https://slack.com/api/users.admin.invite"
+
+#data to be sent to
+data = {'token': xoxp-461571340768-461571341648-461953215104-1233114e019a59bf594243e69a7052b9,
+        'email': harshag@slkconsult.com
+        'channels':GDKM90001}
+
+result = requests.post(url=URL, data=data)
+
+print("Output is:%s" %result)
