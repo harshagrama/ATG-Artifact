4829beda27652bbbaff389207b8747b35db22946
Harshendrakumar N N <harshag@slkconsult.com>
2018-10-25 18:19:32 +0100

    Updaetd token

--- guest_invite.py
+++ guest_invite.py
@@ -10,10 +10,11 @@ print ("Channels ->>%s" %channels)
 URL = "https://slack.com/api/users.admin.invite"
 
 #data to be sent to
-data = {'token': xoxp-461571340768-461571341648-461953215104-1233114e019a59bf594243e69a7052b9,
-        'email': harshag@slkconsult.com
-        'channels':GDKM90001}
+data = {'token': 'xoxp-461571340768-461571341648-465944437334-3d0d32a21acdfda2d235b6cfb783b5e4',
+        'email': 'harshag@slkconsult.com',
+        'channels': 'GDKM90001',
+        'ultimate_restricted': '1'}
 
 result = requests.post(url=URL, data=data)
 
-print("Output is:%s" %result)
+print("Output is:%s" %result.text)
